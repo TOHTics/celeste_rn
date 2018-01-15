43506d856a7ab3143c4f89a9e41481f49631fc1d
Carlos Brito <carlosdotb@gmail.com>
2018-01-15 11:58:16 -0600

    initial commit

---
+++ sunspec.hpp
@@ -0,0 +1,22 @@
+//<editor-fold desc="Preamble">
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (C) 9/6/17 Carlos Brito
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+//</editor-fold>
+
+//<editor-fold desc="Description">
+/**
+ * @file
+ * @brief No description available.
+ */
+//</editor-fold>
+#ifndef SUNSPEC_HPP
+#define SUNSPEC_HPP
+
+#include "data/data.hpp"
+#include "util/util.hpp"
+#include "verifier/verify.hpp"
+
+#endif //SUNSPEC_HPP
